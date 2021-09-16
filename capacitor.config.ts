cbecf94e45c9d4eec6976ea0db5856c8e8eb5d1b
Lucas Machado <lucasmachado@pop-os.localdomain>
2021-09-16 13:40:28 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'maxmin',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
